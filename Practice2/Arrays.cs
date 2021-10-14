1bfa1301dd8bfe18d110d564db007b39f5b57811
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-14 01:40:23 -0400

    Arrays class added

---
+++ Practice2/Arrays.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class Arrays
+    {
+        
+
+        static void Main(string[] args)
+        {
+            // declare 2D Array
+            string[,] matrix;
+
+            // declare 3D Array
+            int[,,] threeD;
+
+            int[,] twoDeeArr = new int[,]
+            {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+            };
+
+            Console.WriteLine("The middle value in the 2D array is {0}\n\n", twoDeeArr[0, 1]);
+
+            int[,,] threeDeeArr = new int[,,]
+            {
+                {
+                    {000,001},
+                    {010,011},
+                    {020, 021}
+                },
+                {
+                    {100,101},
+                    {110,111},
+                    {120,121}
+                }
+            };
+
+            Console.WriteLine("The value of threeDeeArr[1,2,0] is {0}\n\n", threeDeeArr[1,2,0]);
+
+            int dimensions = threeDeeArr.Rank;
+
+            Console.WriteLine("The dimensions of threeDeeArr is: ", dimensions);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Practice2/Challenge1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class Challenge1
+    {
+        static string type;
+/*
+        static void Main(string[] args)
+        {
+
+            stringJawn();
+
+            Console.ReadKey();
+
+        }
+*/
+        static void stringJawn()
+        {
+            bool valid = false;
+            
+
+            Console.WriteLine("Enter a value: ");
+            string inputVal = Console.ReadLine();
+
+            Console.WriteLine("Select the Data type to validate the input you have entered." +
+                "\n---- Press 1 for String" +
+                "\n---- Press 2 for Integer" +
+                "\n---- Press 3 for Boolean" +
+                "\n\nEnter: ");
+            int inputType = Convert.ToInt32(Console.ReadLine());
+
+            switch(inputType)
+            {
+                case 1:
+                    valid = isAString(inputVal);
+                    type = "String";
+                    break;
+
+                case 2:
+                    int intVal;
+                    valid = int.TryParse(inputVal, out intVal);
+                    type = "Integer";
+                    break;
+                case 3:
+                    bool booJawn = false;
+                    valid = bool.TryParse(inputVal, out booJawn);
+                    type = "Boolean";
+                    break;
+                default:
+                    type = "";
+                    Console.WriteLine("\n*****Restart the program and select 1 String, 2 Integer or 3 Boolean*****\n\n");
+                    break;
+            }
+
+            Message(valid);
+
+
+
+        }
+
+        static void Message(bool valid)
+        {
+            if (valid)
+            {
+                Console.WriteLine("The value you entered is a valid {0}", type);
+            } else
+            {
+                Console.WriteLine("The value you entered is invalid");
+            }
+        }
+
+        static bool isAString(string inputVal)
+        {
+            foreach(char c in inputVal)
+            {
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
