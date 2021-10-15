913641836f78d587564808d4e7d14924f3b8222c
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-15 17:12:48 -0400

    TicTacToe complete

--- Practice2/TicTacToe.cs
+++ Practice2/TicTacToe.cs
@@ -16,13 +16,32 @@ namespace Practice2
             {'7','8','9' }
         };
 
-        static char player1 = 'O';
-        static char player2 = 'X';
+        
+        static char currentPlayer = 'O';
+        static bool game = true;
+        static int numTurns = 0;
+        static char win = 'N';
+        
 
 
         static void Main(string[] args)
         {
-            SetField();
+
+
+            while(game)
+            {
+                if(!NewGame())
+                {
+                    break;
+                }
+                Console.Clear();
+                SetField();
+                PlayGame();
+
+                KeepPlaying();
+                
+            }
+            
             Console.ReadKey();
         }
 
@@ -41,68 +60,231 @@ namespace Practice2
             
         }
 
-        public static void playGame()
+
+        
+
+        public static void PlayGame()
         {
+            
+            if(currentPlayer.Equals('O'))
+            {
+                Console.WriteLine("Player 1, pick an available square");
+            } else
+            {
+                Console.WriteLine("Player 2, pick an available square");
+            }
+
+            
+            char choice;
+            string input = Console.ReadLine();
+            bool success = char.TryParse(input, out choice);
+            if(success && choice >= '1' && choice <= '9')
+            {
+                UpdateTable(choice, currentPlayer);
+                    
+            } else
+            {
+                Console.WriteLine("\nPlease enter a number 1-9\n\n");
+                PlayGame();
+            }
 
             
         }
 
 
-        public static bool Player1()
+        public static void UpdateTable(char num, char player)
         {
-            Console.WriteLine("Player 1, pick an available square");
-            string input = Console.ReadLine();
-            char choice;
-            bool success = char.TryParse(input, out choice);
-            if(success)
-            {
-                if(choice >= '1' && choice <= '9')
-                {
-                    UpdateGame(choice);
-                }
-            } else
-            {
-                Console.WriteLine("Please enter a number 1-9");
-            }
-
-            return true;
-        }
-
-
-        public static void UpdateGame(char num)
-        {
+            bool available = false;
             switch(num)
             {
                 case '1':
-                    playField[0, 0] = '0';
+                    if(!playField[0, 0].Equals('O') && !playField[0, 0].Equals('X'))
+                    {
+                        playField[0, 0] = player;
+                        available = true;
+                    }                     
                     break;
                 case '2':
-                    playField[0, 1] = '0';
+                    if (!playField[0, 1].Equals('O') && !playField[0, 1].Equals('X'))
+                    {
+                        playField[0, 1] = player;
+                        available = true;
+                    }
+                    
                     break;
                 case '3':
-                    playField[0, 2] = '0';
+                    if (!playField[0, 2].Equals('O') && !playField[0, 2].Equals('X'))
+                    {
+                        playField[0, 2] = player;
+                        available = true;
+                    }
+                   
+                    
                     break;
                 case '4':
-                    playField[1, 0] = '0';
+                    if (!playField[1, 0].Equals('O') && !playField[1, 0].Equals('X'))
+                    {
+                        playField[1, 0] = player;
+                        available = true;
+                    }
+                   
+                    
                     break;
                 case '5':
-                    playField[1, 1] = '0';
+                    if (!playField[1, 1].Equals('O') && !playField[1, 1].Equals('X'))
+                    {
+                        playField[1, 1] = player;
+                        available = true;
+                    }
+                    
                     break;
                 case '6':
-                    playField[1, 2] = '0';
+                    if (!playField[1, 2].Equals('O') && !playField[1, 2].Equals('X'))
+                    {
+                        playField[1, 2] = player;
+                        available = true;
+                    }
+                    
                     break;
                 case '7':
-                    playField[2, 0] = '0';
+                    if (!playField[2, 0].Equals('O') && !playField[2, 0].Equals('X'))
+                    {
+                        playField[2, 0] = player;
+                        available = true;
+                    }
+                   
+                    
                     break;
                 case '8':
-                    playField[2, 1] = '0';
+                    if (!playField[2, 1].Equals('O') && !playField[2, 1].Equals('X'))
+                    {
+                        playField[2, 1] = player;
+                        available = true;
+                    }
+                    
                     break;
                 case '9':
-                    playField[2, 2] = '0';
+                    if (!playField[2, 2].Equals('O') && !playField[2, 2].Equals('X'))
+                    {
+                        playField[2, 2] = player;
+                        available = true;
+                    }
+                    
                     break;
+            }
+            if(!available)
+            {
+                Console.WriteLine("That square is taken.\n");
+                PlayGame();
+            }
+            
+        }
+
+        public static void SwitchPlayer()
+        {
+            
+            if (currentPlayer == 'O')
+            {
+                currentPlayer = 'X';
+            } else
+            {
+                currentPlayer = 'O';
             }
         }
 
 
+        public static void KeepPlaying()
+        {
+            
+            if(playField[0,0] == 'O' && playField[0, 1] == 'O' && playField[0, 2] == 'O' || playField[1, 0] == 'O' && playField[1, 1] == 'O' && playField[1, 2] == 'O' ||
+                playField[2, 0] == 'O' && playField[2, 1] == 'O' && playField[2, 2] == 'O' || playField[0, 0] == 'O' && playField[1, 0] == 'O' && playField[2, 0] == 'O' ||
+                playField[1, 0] == 'O' && playField[1, 1] == 'O' && playField[1, 2] == 'O' || playField[0, 2] == 'O' && playField[1, 2] == 'O' && playField[2, 2] == 'O' ||
+                playField[0, 0] == 'O' && playField[1, 1] == 'O' && playField[2, 2] == 'O' || playField[0, 2] == 'O' && playField[1, 1] == 'O' && playField[2, 0] == 'O')
+            {
+                Console.WriteLine("Player 1 wins!!! Would you like to play again? Please enter Y to continue or hit any key then enter to exit.");
+                string input = Console.ReadLine();
+                if (input.Equals("Y"))
+                {
+                    numTurns = 0;
+                    win = 'W';
+                } else
+                {
+                    win = 'L';
+                }
+
+            }
+
+            if (playField[0, 0] == 'X' && playField[0, 1] == 'X' && playField[0, 2] == 'X' || playField[1, 0] == 'X' && playField[1, 1] == 'X' && playField[1, 2] == 'X' ||
+                playField[2, 0] == 'X' && playField[2, 1] == 'X' && playField[2, 2] == 'X' || playField[0, 0] == 'X' && playField[1, 0] == 'X' && playField[2, 0] == 'X' ||
+                playField[1, 0] == 'X' && playField[1, 1] == 'X' && playField[1, 2] == 'X' || playField[0, 2] == 'X' && playField[1, 2] == 'X' && playField[2, 2] == 'X' ||
+                playField[0, 0] == 'X' && playField[1, 1] == 'X' && playField[2, 2] == 'X' || playField[0, 2] == 'X' && playField[1, 1] == 'X' && playField[2, 0] == 'X')
+            {
+                Console.WriteLine("Player 2 wins!!!  Would you like to play again? Please enter Y to continue or hit any key then enter to exit.");
+                string input = Console.ReadLine();
+                if (input.Equals("Y"))
+                {
+                    numTurns = 0;
+                    win = 'W';
+                } else
+                {
+                    win = 'L';
+                }
+                
+                
+            }
+
+            numTurns++;
+            SwitchPlayer();
+            
+            
+            
+        }
+
+        public static bool NewGame()
+        {
+
+            bool startOver = true;
+
+            char[,] ticTacToe =
+            {
+                {'1','2','3' },
+                {'4','5','6' },
+                {'7','8','9' }
+            };
+
+
+            if (numTurns == 9)
+            {
+                Console.WriteLine("It's a tie!!!  Would you like to play again??? Please enter Y to continue or hit any key then enter to exit.");
+                string input = Console.ReadLine();
+                if (input.Equals("Y")) {
+                    playField = ticTacToe;
+                    currentPlayer = 'O';
+                } else
+                {
+                    Console.WriteLine("Thank you for playing!!!");
+                    startOver = false;
+                }
+            }
+
+            if (win.Equals('W'))
+            {
+                playField = ticTacToe;
+                currentPlayer = 'O';
+                
+            } else if(win.Equals('L'))
+            {
+                Console.WriteLine("Thank you for playing!!!");
+                startOver = false;
+            }
+
+
+            return startOver;
+
+            
+            
+
+
+        }
     }
 }
