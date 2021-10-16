2d018975d5a8d56e92d5724258a7c393199fcd16
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-15 20:52:03 -0400

    params class

---
+++ Practice2/Params.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class Params
+    {
+
+
+        static void Main(string[] args)
+        {
+            ParamsMethod("Hi,", " My", " name is", " Mike.");
+            Console.ReadKey();
+        }
+
+        private static void ParamsMethod(params string[] sentence)
+        {
+            foreach(string s in sentence)
+            {
+                Console.Write(s);
+            }
+        }
+
+    }
+}
