acefaa7c953ab2d66c5273920e8ad33f6076a3c3
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-20 19:15:20 -0400

    QueueDemo class added

---
+++ Practice2/QueueDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class QueueDemo
+    {
+
+
+        public QueueDemo()
+        {
+            Queue<int> queue = new Queue<int>();
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
